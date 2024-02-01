8c056dc7b011810d63da15dedacc3036313c1ca5
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-02-01 21:24:52 +0100

    fix: update changed lom api

--- invenio_global_search/serializers/lom/schema.py
+++ invenio_global_search/serializers/lom/schema.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023 Graz University of Technology.
+# Copyright (C) 2023-2024 Graz University of Technology.
 #
 # invenio-global-search is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 """Global Search LOM schema."""
 
-from invenio_records_lom.resources.serializers.schemas import (
+from invenio_records_lom.resources.serializers.dublincore.schema import (
     LOMToDublinCoreRecordSchema,
 )
 
